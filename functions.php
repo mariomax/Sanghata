551d7dc0f56afb2d03a05e914066218c11a442e8
Mario Lopriore <mariojames211@gmail.com>
2019-06-25 13:36:53 -0700

    functions.php called stylesheet twice. CSS edits.

--- functions.php
+++ functions.php
@@ -6,7 +6,9 @@ function enqueue_wp_child_theme()
 {
     wp_enqueue_style('parent-css', get_template_directory_uri() . '/style.css');
 
-    wp_enqueue_style('child-css', get_stylesheet_uri());
+    // wp_enqueue_style('child-css', get_stylesheet_uri());
+    // This is redundant and causes the child theme's stylesheet to be loaded twice!
+    // The child theme's stylesheet is automatically being loaded.
 
     wp_enqueue_script('child-js', get_stylesheet_directory_uri() . '/js/script.js', array('jquery'), '1.0', true);
 }
